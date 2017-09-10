7b0a8c4b0e203fc7d06552ff58dff714b352b50a
Maximilian Remming <maxremming@gmail.com>
2017-09-10 15:47:51 +0300

    Added requirements

--- setup.py
+++ setup.py
@@ -11,8 +11,11 @@ setup(
     author="Maximilian Remming",
     author_email="maxremming@gmail.com",
     url="https://github.com/PolarPayne/scrycli",
-    download_url="https://github.com/PolarPayne/scrycli/archive/{}.tar.gz".format(VERSION),
+    download_url="https://github.com/PolarPayne/scrycli/archive/{}.tar.gz".format(__version__),
     license="MIT",
+    install_requires=[
+        "requests==2.18.4"
+    ],
     entry_points={
           'console_scripts': [
               'scrycli = scrycli.__main__:main'
